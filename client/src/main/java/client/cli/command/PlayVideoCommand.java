9a9384b30fa116e9fd180f9a4183d96a79dc66e8
japs <japslv@gmail.com>
2017-10-28 12:46:14 +0100

    Add the command to play the video
    Now using play video command a ffplay windows plays the vido.

--- client/src/main/java/client/cli/command/PlayVideoCommand.java
+++ client/src/main/java/client/cli/command/PlayVideoCommand.java
@@ -2,7 +2,12 @@ package client.cli.command;
 
 import client.cli.Command;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import java.lang.Object;
 
 /**
  * Created by satyan on 10/8/17.
@@ -24,4 +29,72 @@ public class PlayVideoCommand extends Command {
         System.out.println(getClass().getName());
     }
 
+    public static class Video{
+        private final String name;
+        private final String directory;
+        private final List<String> keywords;
+        private final int bitrate;
+        private final String size;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDirectory() {
+            return directory;
+        }
+
+        public List<String> getKeywords() {
+            return keywords;
+        }
+
+        public int getBitrate() {
+            return bitrate;
+        }
+
+        public String getSize() {
+            return size;
+        }
+
+        public Video(String name, String directory, List<String> keywords, int bitrate, String size){
+            this.name = name;
+            this.directory = directory;
+            this.keywords = keywords;
+            this.bitrate = bitrate;
+            this.size = size;
+        }
+
+    }
+
+    public static void main(String args[]){
+        List<String> kw = new ArrayList<>();
+        kw.add("Comedy");
+        Video video = new Video("Popeye", "/home/japs/Desktop/PopeyeAliBaba_512kb.mp4", kw, 63, "321x240");
+
+        playVideo(video);
+    }
+
+    public static void playVideo(Video video){
+        List<String> cmdlist = new ArrayList<String>();
+        cmdlist.add("man");
+        //cmdlist.add("ffplay https://ia600306.us.archive.org/35/items/PopeyeAliBaba/PopeyeAliBaba_512kb.mp4");
+
+        ProcessBuilder pb = new ProcessBuilder("ffplay", video.getDirectory());
+       //ProcessBuilder pb = new ProcessBuilder("ls");
+        File log = new File("log");
+        pb.redirectErrorStream(true);
+        pb.redirectOutput(ProcessBuilder.Redirect.appendTo(log));
+        try {
+            Process p = pb.start();
+            assert pb.redirectInput() == ProcessBuilder.Redirect.PIPE;
+            assert pb.redirectOutput().file() == log;
+            assert p.getInputStream().read() == -1;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
+
